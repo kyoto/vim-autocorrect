f41bef61098d25ff32478687844024f37363b618
Andy <andy.nihon@gmail.com>
2010-11-15 00:43:53 +0900

    Add several extra RSpec tests

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -2,7 +2,11 @@ require_relative "../generator"
 
 describe "generator" do
   it "valid_lhs?() determines whether a sting is a valid VIM iabbrev LHS" do
+    valid_lhs?("a").should be_true
+    valid_lhs?("A").should be_true
+    valid_lhs?("1").should be_true
     valid_lhs?("abc").should be_true
+    valid_lhs?("aB3").should be_true
     valid_lhs?("a bc").should be_false
     valid_lhs?("a\tbc").should be_false
     valid_lhs?(" abc").should be_false
